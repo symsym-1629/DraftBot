6eeeb8b23a96691757e11a21890b258506dff0f3
BastLast <bastien.last87@gmail.com>
2021-07-08 19:23:41 +0200

    ajout de stats

--- src/commands/player/LimiterCommand.js
+++ src/commands/player/LimiterCommand.js
@@ -7,6 +7,8 @@
 const LimiterCommand = async function (language, message, args) {
 	let limiters = await FightLimiter.getforUser(message.author.id);
 	let description = "";
+	let count = 0;
+	let adv = 0;
 	for (let i = 0; i < limiters.length; i++) {
 		let id;
 		if (limiters[i].smallid === message.author.id) {
@@ -16,11 +18,18 @@ const LimiterCommand = async function (language, message, args) {
 		}
 		[entity] = await Entities.getOrRegister(id);
 		let pseudo = await entity.Player.getPseudo(language);
+		adv++;
+		count+=limiters[i].amount;
 		description += "**" + pseudo + "** : " + limiters[i].amount + "/3\n";
 	}
 
 	if (description.length === 0) {
 		description = "Aucun combat n'a été réalisé pour le moment.";
+	}else{
+		description += "\n**Nombre total de combats :** `" + count+
+			"`\n**Nombre théorique de combats possibles restants :** `" + (61*3 -count)+
+			"`\n**Nombre d'adversaires rencontrés :** `" + adv+
+			"`\n**Nombre d'adversaires jamais combattus :** `" + (62 - adv)+"`";
 	}
 
 	const limiterEmbed = new discord.MessageEmbed()
